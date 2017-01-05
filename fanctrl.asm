91f16445cd07000e89dba87474cb257dc3225d61
Chaoyang Liu <chaoyanglius@gmail.com>
2017-01-05 14:20:40 +0800

    Resolve #4
    只用8255，不用P3口。将P3口的工作交给8255的PC口完成。

--- fanctrl.asm
+++ fanctrl.asm
@@ -1,12 +1,13 @@
-		pa_add equ 0ff20h				
-		pb_add equ 0ff21h
-		pc_add equ 0ff22h
-		pcon_add equ 0ff23h
+		pa_add equ 0ff28h				
+		pb_add equ 0ff29h
+		pc_add equ 0ff2ah
+		pcon_add equ 0ff2bh
 		int_times equ 0ah
 		buf_add equ 79h
 		gear equ 50h		;存储档位
 		temp equ 51h
-		;myflag bit 2fh  
+		dac0832_add equ 0ff80h
+		haveCount bit 2dh  
 		
 		org 0000h
 		ljmp main
@@ -21,7 +22,7 @@
 main:	mov sp,#30h				;设置栈顶
 		mov r0,#0
 		mov dptr,#pcon_add		;初始化8255	 
-		mov a,#81h
+		mov a,#80h
 		movx @dptr,a
 		mov p1,#0ffh			;将p1作为输入
 		mov buf_add+4,#0ah
@@ -35,46 +36,46 @@ main:	mov sp,#30h				;设置栈顶
 		mov tl1,#0fh
 		mov ie,#8ah				;允许定时器0和定时器1中断
 		setb pt0				;设置定时器1为高优先级
-		setb tr0		;test
-		mov r1,#60		;test
+		;setb tr0		;test
+		;mov r1,#60		;test
 		setb tr1				;启动定时器1				
-		setb f0
-		;clr myflag		
-				
-							
+		clr f0					;开关机状态标志，默认关机
+		clr haveCount					
 
 ;主程序		
 here:	nop
-		;jnb p1.7,here			;检测是否打开开关
+		mov dptr,#dac0832_add
+		mov a,#0				;关闭电动机
+		movx @dptr,a		
+		jnb p1.7,here			;检测是否打开开关
+		jnb p1.6,setTime			;检测是否设置定时
+		jnb haveCount,hset
+		jnb f0,here
 		;jb myflag,hset
-		;jb p1.6,setTime			;检测是否设置定时
-		;clr myflag
+				
 hset:	mov a,p1
 		anl a,#00000011b			;取低两位作为档位
-		mov gear,a
-		mov dptr,#gear_led		;根据档位设置LED灯
-		movc a,@a+dptr
-		mov p3,a						
-		;call motor_driv		  				
+		mov gear,a							
+		call motor_driv		  				
 		jmp here
 ;定时时间设置
-setTime:;setb myflag 
-again:	  
+setTime: 
+again:	mov r1,#60  
 
-		jnb p1.6,again
-	   	setb tr0				;打开定时器0
+		jb p1.6,again
+	   	setb tr0			;打开定时器0
+		setb f0				
+		setb haveCount
 		ret
 ;中断服务子程序0，主要负责定时时间的修改		
-isr_t0: push psw
-		push acc
-
+isr_t0: push acc
 		jnb f0,ret0			;判断标志位
 		mov th0,#0bh
 		mov tl0,#0cdh
-		mov a,30h
-		dec a
-		mov 30h,a
-		jnz ret0
+		mov r6,30h
+		dec r6
+		mov 30h,r6
+		cjne r6,#0,ret0
 		
 		mov 30h,#int_times		;重置中断次数
 		mov a,r1
@@ -82,20 +83,19 @@ isr_t0: push psw
 		div ab
 		mov buf_add+0,b
 		mov buf_add+1,a
-
 check0:	cjne r1,#0,dec_f 		;到达定时时间
-		;clr f0					;设置标志位
+		clr f0					;设置标志位
+		clr tr0
 		jmp ret0				
 dec_f:	dec r1
-		pop acc
-		pop psw
-ret0:	reti
+ret0:	pop acc
+		reti
 
 ;中断处理子程序1，主要负责实时显示7段数码管
-disp:	push psw
-		push acc
+disp:	push acc
 		push dpl
 		push dph
+		push 3
 		mov r3,#0feh	  ;存放位码
 		mov r0,#buf_add	  ;存放段码的地址
 loop1:	mov a,@r0	
@@ -123,19 +123,26 @@ loop2:	nop
 		jnb acc.5,ret1		;判断是否扫描到第5位
 		rl a				;移位以扫描下一位
 		mov r3,a
+		mov a,gear
+		mov dptr,#gear_led		;根据档位设置LED灯
+		movc a,@a+dptr
+		mov dptr,#pc_add
+		movx @dptr,a
 		jmp loop1
+		
+ret1:	pop 3
 		pop dph
 		pop dpl
-		pop acc
-		pop psw		
-ret1:	reti
+		pop acc	
+		reti
 ;电动机驱动程序
-motor_driv:	;push acc
+motor_driv:	jnb f0,next_ch
+next_ch:	jb haveCount,ret2			
 			mov dptr,#gear_value
 			mov a,gear			;以gear作为偏移量取出档位
 			movc a,@a+dptr
-			mov r3,a			;延时2的时间
-			mov dptr,#0ff80h
+			mov r4,a			;延时2的时间
+			mov dptr,#dac0832_add
 			mov a,#80h
 			movx @dptr,a
 			mov temp,#150		;延时1的时间
@@ -150,9 +157,8 @@ delay2:		nop
 			nop
 			nop
 			nop
-			djnz r3,delay2
-			;pop acc
-			ret
+			djnz r4,delay2
+ret2:		ret
 ;将定时时间置位，即全设置为横杠
 reset:		mov buf_add,#11
 			mov buf_add+1,#11
@@ -163,5 +169,5 @@ reset:		mov buf_add,#11
 disdata: db 0c0h,0f9h,0a4h,0b0h,99h,92h,82h,0f8h,80h,90h,0ffh
 reset_sym: db 0bfh					;不设置定时时，7段数码管显示的符号
 gear_value: db 5,75,150,0  			;四个档位值
-gear_led:	db 7fh,0bfh,0dfh,0efh	;四个档位对应的LED灯
+gear_led: db 7fh,0bfh,0dfh,0efh	;四个档位对应的LED灯
 		end	
