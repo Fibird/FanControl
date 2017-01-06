0f45b2535041081b34d61e197796135c464beb12
Chaoyang Liu <chaoyanglius@gmail.com>
2017-01-06 15:40:45 +0800

    改善代码质量
    为了提高可读性，我添加了必要的注释；另外，也加了版权声明、相关协议以及README。

--- src/fanctrl.asm
+++ src/fanctrl.asm
@@ -1,37 +1,45 @@
+;*************************************************
+;Copyright(c) 2017 西北农林科技大学信息工程学院
+;名称：电风扇控制程序
+;作者：刘朝洋
+;*************************************************
+;================各端口地址====================
 		pa_add equ 0ff28h				
 		pb_add equ 0ff29h
 		pc_add equ 0ff2ah
 		pcon_add equ 0ff2bh
 		dac0832_add equ 0ff80h
-		int_times equ 0ah
-		low_times equ 150
-		count_value equ 60h
-		buf_add equ 79h
+;===============常用内存空间===================
+		int_times equ 0dh	;中断int_times次为1秒
+		low_times equ 150	;低脉冲次数
+		count_value equ 60h	;计数值缓冲区
+		buf_add equ 79h		;数码管缓冲区首址
 		gear equ 50h		;存储档位
 		temp equ 51h		;临时存储区
-		bitbuff0 bit 00h		;位缓冲区
+		bitbuff0 bit 00h	;位缓冲区
 		bitbuff1 bit 01h
 		bitbuff2 bit 02h  
-		E bit 20h
+		E bit 20h			;异或程序的参数
 		F bit 21h
 		NOE bit 22h
 		NOF bit 23h
-
+;==============================================
 		org 0000h
 		ljmp main
-
-		org 000bh
+;=============中断向量表=================
+		org 000bh		;定时器0中断
 		ljmp isr_t0
-
-		org 001bh
+		org 001bh		;定时器1中断
 		ljmp motor_driv
+;========================================
+
 ;***************主程序*********************
 ;完成各种定时器的初始化设置，中断及其优先
 ;级的设置，输入输出控制的设置以及8255的初
 ;始化设置。
 ;******************************************		
 		org 0030h
-;初始化设置
+;-----------------初始化设置----------------
 main:	mov sp,#30h				;设置栈顶
 		mov r0,#0
 		mov dptr,#pcon_add		;初始化8255	 
@@ -42,7 +50,7 @@ main:	mov sp,#30h				;设置栈顶
 		mov buf_add+5,#0ah
 	
 		mov 30h,#int_times		;设置中断次数
-		mov temp+1,#low_times			;低电平次数	
+		mov temp+1,#low_times	;低电平次数	
 		mov tmod,#21h	 
 		mov th0,#0bh			;初始化定时器0	 
 		mov tl0,#0cdh
@@ -54,9 +62,9 @@ main:	mov sp,#30h				;设置栈顶
 		setb tr0				;打开定时器0
 		setb tr1				;启动定时器1				
 		clr f0					;开关机状态标志，默认关机
-		mov c,p1.5				
-		mov bitbuff0,c
-		mov c,p1.4
+		mov c,p1.5				;保存初始开关状态
+		mov bitbuff0,c			
+		mov c,p1.4				;保存初始开关状态
 		mov bitbuff1,c					
 ;-----------------------------
 ;循环检测开关，并扫描七段数码管
@@ -76,7 +84,7 @@ set_t: 	mov c,p1.5				;定时时间设置
 		mov NOF,c
 		call bxrl
 		jnc next_b
-		inc count_value		
+		inc count_value			;开关变化计数值加1	
 		mov c,p1.5
 		mov bitbuff0,c
 next_b:	mov c,p1.4
@@ -89,22 +97,22 @@ next_b:	mov c,p1.4
 		mov NOF,c
 		call bxrl
  		jnc goon
-		dec count_value
+		dec count_value			;开关变化计数值减1
 		mov c,p1.4
 		mov bitbuff1,c
-goon:  	setb tr0			;打开定时器0				
+goon:  	setb tr0				;打开定时器0				
 		jmp get_g						
 noset:	call reset
 get_g:	mov a,p1				;读取档位
 		anl a,#00000011b		;取低两位作为档位
 		mov gear,a							
-		call disp		  				
+		call disp				;扫描七段数码管和LED灯		  				
 		jmp here
 ;-------------------------------
 ;**************位异或程序*****************
-;功能:对两个位进行异或
-;输入参数:E，F, NOE, NOF
-;返回参数:CY
+;功能：对两个位进行异或
+;输入参数：E，F, NOE, NOF
+;输出参数：CY
 ;*****************************************
 bxrl:	mov c,F
 		anl c,NOE
@@ -113,13 +121,17 @@ bxrl:	mov c,F
 		anl c,NOF
 		orl c,bitbuff2
 		ret
-;***********计时程序*********************
-;中断服务子程序0，主要负责定时时间的修改
-;
-;****************************************		
-isr_t0: push acc
-		push psw
 
+;**************定时器0的中断服务子程序*****************
+;功能：每隔1s修改计数值
+;输入参数：count_value
+;输出参数：buf_add+0， buf_add+1
+;******************************************************		
+isr_t0: 
+;------------保护现场--------------
+		push acc
+		push psw
+;----------------------------------
 		mov r1,count_value		;更新缓冲区内的值
 		mov a,r1
 		mov b,#10
@@ -136,23 +148,21 @@ isr_t0: push acc
 		cjne r6,#0,ret0
 
 next1:	mov 30h,#int_times		;重置中断次数
-		;mov r1,count_value
-		;mov a,r1
-		;mov b,#10
-		;div ab
-		;mov buf_add+0,b
-		;mov buf_add+1,a
+
 check0:	cjne r1,#0,dec_f 		;到达定时时间
 		jmp ret0				
 dec_f:	dec r1
 		mov count_value,r1
+;------------恢复现场--------------
 ret0:	pop psw
 		pop acc
 		reti
-;**************七段数码管扫描程序****************
-;主要负责扫描七段数码管的保证计时时间的正常显示，
-;另外也负责扫描LED灯，保证档位的正常显示
-;************************************************
+;********************七段数码管扫描程序**********************
+;功能：主要负责扫描七段数码管的保证计时时间的正常显示，另外也
+;负责扫描LED灯，保证档位的正常显示
+;输入参数：buf_add
+;输出参数：无
+;************************************************************
 disp:	mov r3,#0feh	  ;存放位码
 		mov r0,#buf_add	  ;存放段码的地址
 loop1:	mov a,@r0	
@@ -164,20 +174,9 @@ loop1:	mov a,@r0
 		mov a,r3			  
 		movx @dptr,a		;发送位码 
 
-;delay0:	mov r5,#10h		 	;延时0
-;loop2:	nop
-;		djnz r5,loop2
-		
-		;mov dptr,#pb_add	
-		;mov a,#0ffh
-		;movx @dptr,a
 		mov a,#0ffh			;消隐
 		mov dptr,#pa_add
 		movx @dptr,a
-
-;		mov r5,#10h		 	;延时0
-;loop3:	nop
-;		djnz r5,loop3
 
 		inc r0
 		mov a,r3 
@@ -192,16 +191,18 @@ loop1:	mov a,@r0
 		jmp loop1
 		
 ret1:	ret
-;***********电动机驱动程序**************
-;中断服务子程序2，主要负责驱动电动机
-;
-;***************************************
+;***********定时器1中断服务子程序**************
+;功能：主要负责驱动电动机
+;输入参数：gear
+;输出参数：无
+;**********************************************
 motor_driv:	
 ;------------保护现场--------------
 			push acc
 			push dpl
 			push dph
 			push psw
+;----------------------------------
 			jnb f0,mstop
 			jnb p1.6,movit
 			mov a,count_value
@@ -241,13 +242,18 @@ send_h:		mov dptr,#gear_value
 mstop:		mov dptr,#dac0832_add
 			mov a,#0h
 			movx @dptr,a
-;------------中断返回--------------
+;------------恢复现场--------------
 ret2:		pop psw
 			pop dph
 			pop dpl
 			pop acc
 			reti
-;将定时时间置位，即全设置为横杠
+
+;***********重置子程序*************************
+;功能：清除显示缓冲区，关闭定时器0
+;输入参数：无
+;输出参数：无
+;**********************************************
 reset:		clr tr0
 			mov count_value,#60
 			mov buf_add,#11
@@ -255,9 +261,10 @@ reset:		clr tr0
 			mov buf_add+2,#11
 			mov buf_add+3,#11
 			ret 
-
+;====================数据区===========================
 disdata: db 0c0h,0f9h,0a4h,0b0h,99h,92h,82h,0f8h,80h,90h,0ffh	;数码管字形码
 reset_sym: db 0bfh					;不设置定时时，7段数码管显示的符号
 gear_value: db 5,75,150,0  			;四个档位值
 gear_led: db 7fh,0bfh,0dfh,0efh	;四个档位对应的LED灯
+;=====================================================
 		end	
